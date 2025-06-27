1e7aef112229dad22340607433b87731473e5952
chanwime <chanwime@tlt.co.th>
2025-06-27 23:45:42 +0700

    initial

--- App.tsx
+++ App.tsx
@@ -6,13 +6,28 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import Config from 'react-native-config';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
+      <Text
+        style={{
+          marginTop: 50,
+          alignItems: 'center',
+        }}
+      >
+        {Config.ENV}
+      </Text>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NewAppScreen templateFileName="App.tsx" />
     </View>
